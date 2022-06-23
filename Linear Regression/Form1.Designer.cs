fdd004f0d3bf3999904f4e74b3856ffcd31b7a80
F_Zamzami <68177295+pentolKuda@users.noreply.github.com>
2022-06-23 21:08:04 +0800

    first commit

---
+++ Linear Regression/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace Linear_Regression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupEquation = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInputNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupEquation.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupEquation
+            // 
+            this.groupEquation.Controls.Add(this.label9);
+            this.groupEquation.Controls.Add(this.label8);
+            this.groupEquation.Controls.Add(this.label7);
+            this.groupEquation.Controls.Add(this.label4);
+            this.groupEquation.Controls.Add(this.label3);
+            this.groupEquation.Controls.Add(this.label1);
+            this.groupEquation.Controls.Add(this.dataGridView1);
+            this.groupEquation.Controls.Add(this.button2);
+            this.groupEquation.Controls.Add(this.label6);
+            this.groupEquation.Controls.Add(this.label5);
+            this.groupEquation.Location = new System.Drawing.Point(34, 163);
+            this.groupEquation.Name = "groupEquation";
+            this.groupEquation.Size = new System.Drawing.Size(486, 567);
+            this.groupEquation.TabIndex = 12;
+            this.groupEquation.TabStop = false;
+            this.groupEquation.Text = "Equation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 457);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Formula:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(316, 314);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 32);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Create Equation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matrix Size Equation:\r\n";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbInputNumber);
+            this.groupBox2.Location = new System.Drawing.Point(34, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(389, 118);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Equation Setup";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(201, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Number of Data";
+            // 
+            // tbInputNumber
+            // 
+            this.tbInputNumber.Location = new System.Drawing.Point(201, 33);
+            this.tbInputNumber.Name = "tbInputNumber";
+            this.tbInputNumber.Size = new System.Drawing.Size(100, 22);
+            this.tbInputNumber.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 481);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Standar Error";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 508);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Correlation Coefficient";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(186, 481);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(186, 508);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 780);
+            this.Controls.Add(this.groupEquation);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Linear Regression";
+            this.groupEquation.ResumeLayout(false);
+            this.groupEquation.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupEquation;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInputNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Linear Regression/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linear_Regression
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dataCount;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataCount = int.Parse(tbInputNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Masukkan nilai dengan benar!");
+                return;
+            }
+            tbInputNumber.Text = "";
+
+            label6.Text = dataCount.ToString() + " X " + dataCount.ToString();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            /*dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();*/
+
+            //input x and y
+            for (int i = 0; i <= 1; i++)
+            {
+                var column = new DataGridViewColumn();
+                column.HeaderText = (i == 1 ? "Y" : "X" );
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                column.Width = 50;
+                dataGridView1.Columns.Add(column);
+            }
+
+
+
+            for (int i = 0; i < dataCount; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] x_point = new double[dataCount];
+            double[] y_point = new double[dataCount];
+
+            for (int i = 0; i < dataCount; i++)
+            {
+                x_point[i] = double.Parse((string)dataGridView1.Rows[i].Cells[0].Value);
+                y_point[i] = double.Parse((string)dataGridView1.Rows[i].Cells[1].Value);
+                /*Console.Write(x_point[i]); Console.Write("\t");
+                Console.Write(y_point[i]);
+                Console.WriteLine();*/
+            }
+            linear_regression(x_point, y_point);
+
+
+
+        }
+
+        private void linear_regression(double[] _x_point, double[] _y_point)
+        {
+            int _data_count = _x_point.GetLength(0);
+            double sumx = 0, sumy = 0, sumxy = 0, sumx2 = 0, st = 0, sr =0 ;
+
+            for(int i= 0; i< _data_count; i++)
+            {
+                sumx += _x_point[i];
+                sumy += _y_point[i];
+                sumxy += (_x_point[i]*_y_point[i]);
+                sumx2 += (_x_point[i] * _x_point[i]);
+            }
+
+            double xm = sumx / _data_count;
+            double ym = sumy / _data_count;
+            Console.WriteLine(xm);
+            Console.WriteLine(ym);
+
+            double a1 = ((_data_count * sumxy) - (sumx * sumy)) / ((_data_count * sumx2) - (sumx * sumx));
+            double a0 = ym - a1 * xm;
+            Console.WriteLine(a1);
+            Console.WriteLine(a0);
+            // y = a0+a1*x
+
+            for (int i = 0; i < _data_count; i++)
+            {
+                st += ((_y_point[i] - ym) * (_y_point[i] - ym));
+                sr += ((_y_point[i] - a1 * _x_point[i] - a0) * (_y_point[i] - a1 * _x_point[i] - a0));
+            }
+            double syx = Math.Sqrt((sr / (dataCount - 2)));
+            double r2 = (st - sr) / st;
+            double r = Math.Sqrt(r2);
+            Console.WriteLine();
+            Console.WriteLine(r2);
+            Console.WriteLine(r);
+
+            //show on user interface
+            if (a0 > 0)
+            {
+                String formula = "y = " + a1.ToString() + "x +" + a0.ToString();
+                label3.Text = formula;
+            }
+            else if(a0 <0)
+            {
+                String formula = "y = " + a1.ToString() + "x " + a0.ToString();
+                label3.Text = formula;
+            }else if(a0 == 0)
+            {
+                String formula = "y = " + a1.ToString() + "x ";
+                label3.Text = formula;
+            }
+            label8.Text = syx.ToString();
+            label9.Text = r.ToString();
+
+            
+
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
